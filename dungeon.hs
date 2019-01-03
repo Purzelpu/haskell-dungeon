47e9d337af6f9f97b792eb1e695065907a72d7fe
Ingwar Petersen <ingwar@ingwar-petersen.de>
2019-01-03 12:43:24 +0100

    added Player class

--- dungeon.hs
+++ dungeon.hs
@@ -1,6 +1,7 @@
 import Data.Maybe
 
 data Raum = Raum {nord :: Maybe Raum, ost :: Maybe Raum, sued ::  Maybe Raum, west :: Maybe Raum, schatz::Bool, text::String, creature :: Maybe Creature} deriving(Show)
+data Hero = Hero {position :: Raum, message :: String}
 
 data Creature = Orc deriving(Show)
 
@@ -27,17 +28,14 @@ describeRoom r = case r of
     Raum _ _ _ _ True t Nothing ->
                  t ++ " Hier ist der Schatz. Und niemand, der auf ihn aufpasst."
 
-
-describeDoor :: Raum -> String
-describeDoor x = "eine Tuer"
-
 describeExit :: Maybe Raum -> String
-describeExit x = (maybe "eine Wand" describeDoor x)
+describeExit Nothing = "eine Wand"
+describeExit (Just _) = "eine Tuer"
 
-move :: Raum -> Char -> (Raum,String)
+move :: Raum -> Char -> Hero
 move room dirChar = case (getDirection dirChar) room of
-                      Nothing -> (room, "Hier ist keine Geheimtuer.")
-                      Just x -> (x, getFeedback dirChar)
+                      Nothing -> Hero room "Hier ist keine Geheimtuer."
+                      Just x ->  Hero x (getFeedback dirChar)
 
 getDirection :: Char -> (Raum -> Maybe Raum)
 getDirection 'N' = nord
@@ -52,20 +50,21 @@ getFeedback 'S' = "Du gehst nach Sueden"
 getFeedback 'O' = "Du gehst nach Osten"
 getFeedback 'W' = "Du gehst nach Westen"
 
-gameLoop :: Raum -> IO ()
-gameLoop r = case r of 
+gameLoop :: Hero -> IO ()
+gameLoop h = let r = position h in
+    case r of 
     Raum _ _ _ _ False _ (Just c) -> do
        putStrLn $ describeRoom r
        --TODO: Kampf oder Flucht?
        putStrLn $ attackCreature c 
-       gameLoop r{creature=Nothing}
+       gameLoop h{position = r{creature=Nothing}}
     Raum _ _ _ _ False _ Nothing -> do
        putStrLn $ describeRoom r
        putStrLn "Wohin jetzt?"
        dirLine <- getLine
        putStrLn ""
-       putStrLn $ snd $ move r (head dirLine)
-       gameLoop $ fst $ move r (head dirLine)
+       putStrLn $ message $ move r (head dirLine)
+       gameLoop $ move r (head dirLine)
     Raum _ _ _ _ True _ Nothing -> do 
        putStrLn $ describeRoom r
        putStrLn "Du hast den Schatz gefunden!"
@@ -73,7 +72,7 @@ gameLoop r = case r of
 
 
 main = do 
-        gameLoop start
+        gameLoop (Hero start "")
         where start = Raum (Just raum2) Nothing (Just schatz) Nothing False "Der Eingang des Dungeons." Nothing
               schatz = Raum (Just start) Nothing Nothing Nothing True "Die Schatzkammer" Nothing
               raum2 = Raum Nothing (Just raum1) (Just start) Nothing False "Ein kleiner Raum." Nothing
